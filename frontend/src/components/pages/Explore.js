bd56ad8cf018cf4d0a93f193edbe753d6e44f36f
Jamie Wise <wisejamie2@gmail.com>
2023-11-18 14:06:15 -0500

    implementing explore page

--- frontend/src/components/pages/Explore.js
+++ frontend/src/components/pages/Explore.js
@@ -1,6 +1,110 @@
 import React from "react";
+import Card from "../Card"; // Adjust the import path based on your project structure
 import "../../App.css";
+import "../Explore.css";
 
 export default function Explore() {
-  return <h1 className="explore">EXPLORE</h1>;
+  const cardData = [
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "John Doe",
+      researchInterests: "Machine Learning",
+      educationLevel: "Ph.D.",
+    },
+    {
+      type: "Advisor",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Jane Smith",
+      researchInterests: "Artificial Intelligence",
+      educationLevel: "Professor",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+    {
+      type: "Student",
+      profilePicture: "../../../images/profile-icon.jpg",
+      name: "Alice Johnson",
+      researchInterests: "Data Science",
+      educationLevel: "Master's",
+    },
+  ];
+
+  // Function to chunk the array into rows of specified size
+  const chunkArray = (array, size) => {
+    return array.reduce(
+      (acc, _, i) => (i % size ? acc : [...acc, array.slice(i, i + size)]),
+      []
+    );
+  };
+
+  // Organize cards into rows with a maximum of 3 cards per row
+  const rows = chunkArray(cardData, 3);
+
+  return (
+    <div className="explore">
+      <h1>EXPLORE</h1>
+      {rows.map((row, rowIndex) => (
+        <div key={rowIndex} className="card-container">
+          {row.map((card, index) => (
+            <Card key={index} {...card} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
