62898492dcc99a02cd0b9e25f133b91740c0c6be
Marlo Anzarut <marloanzarut@gmail.com>
2023-11-18 15:06:26 -0500

    edited homepage

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video-1.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>DISCOVER, LEARN, THRIVE</h1>
+      <p>YOUR ADVISOR AWAITS</p>
       <div className="hero-btns">
         <Button
           className="btns"
